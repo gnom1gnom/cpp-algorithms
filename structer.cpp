c6fd5bda2cd29572affa69eef6ddee01a1098659
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2021-10-12 06:48:39 +0000

    struct init

--- structer.cpp
+++ structer.cpp
@@ -16,4 +16,8 @@ int main() {
   neuromancer.name = "dupa";
   printf("Neuromancer has %d pages.\n", neuromancer.pages); 
   printf("Neuromancer has %s name.\n", neuromancer.name.c_str()); 
+
+  // initialization of struct in one line
+  Book bible = {"Biblia naszego Pana Jezusa Chrystusa Króla Wszechsiawiata i Zaświata", 0, 2343, true};
+  cout << bible.name << " powstała w " << bible.year << " roku. Ma " << bible.pages << " storn, opowiadających o miłosierdzi naszego Pana.\n";
 }
