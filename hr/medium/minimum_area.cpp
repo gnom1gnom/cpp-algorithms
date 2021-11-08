fdd5102009205eb48bf8f3b721d88a4d54734f3b
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2021-11-08 11:14:14 +0000

    7/10

--- hr/medium/minimum_area.cpp
+++ hr/medium/minimum_area.cpp
@@ -19,31 +19,52 @@ long minArea(vector<int> x, vector<int> y, int k)
 {
     // szukamy lewej dolnej krawędzi
     // szukamy prawej górnej krawedzi
-    int lowerX{INT_MAX}, lowerY{INT_MAX};
-    int upperX{INT_MIN}, upperY{INT_MIN};
 
     vector<pair<int, int>> vect;
     for (int i = 0; i < x.size(); i++)
     {
-        if (x[i] < lowerX)
-            lowerX = x[i] - 1;
-        if (x[i] > upperX)
-            upperX = x[i] + 1;
-
-        if (y[i] < lowerY)
-            lowerY = y[i] - 1;
-        if (y[i] > upperY)
-            upperY = y[i] + 1;
-
         vect.push_back(make_pair(x[i], y[i]));
     }
 
     sort(vect.begin(), vect.end());
 
-    long bok = ((upperX - lowerX) > (upperY - lowerY)) ? (upperX - lowerX) : (upperY - lowerY);
-    return bok * bok;
+    pair<int, int> lowerBig = vect.front();
+    pair<int, int> upperBig = vect.back();
 
-    // obszar zawężamy do k punktów
+    long bokBig = ((upperBig.first - lowerBig.first) > (upperBig.second - lowerBig.second)) ? (upperBig.first - lowerBig.first) : (upperBig.second - lowerBig.second);
+
+    cout << "large square: " << pow(bokBig + 2, 2) << endl;
+
+    vector<pair<int, int>> vect_small;
+    int nextIndex = vect.size() / 2;
+
+    // odejmujemy punk lower albo upper 
+    size_t lowerInd{0}, upperInd{vect.size() - 1};
+    for (int i = vect.size(); i > k && upperInd > lowerInd; i--)
+    {
+        // kwadrat mniejszy o punkt z dołu
+        pair<int, int> lower1 = vect[lowerInd];
+        pair<int, int> upper1 = vect[upperInd - 1];
+        long bok1 = ((upper1.first - lower1.first) > (upper1.second - lower1.second)) ? (upper1.first - lower1.first) : (upper1.second - lower1.second);
+
+        // kwadrat mniejszy o punkt z góry
+        pair<int, int> lower2 = vect[lowerInd + 1];
+        pair<int, int> upper2 = vect[upperInd];
+        long bok2 = ((upper2.first - lower2.first) > (upper2.second - lower2.second)) ? (upper2.first - lower2.first) : (upper2.second - lower2.second);
+
+        // sprawdzamy który ma mniejszy bok
+        // odrzucamy wierdzłołek który generuje większy bok
+        (bok1 >= bok2) ? lowerInd++ : upperInd--;
+    }
+
+    pair<int, int> lower = vect[lowerInd];
+    pair<int, int> upper = vect[upperInd];
+
+    long bok = ((upper.first - lower.first) > (upper.second - lower.second)) ? (upper.first - lower.first) : (upper.second - lower.second);
+
+    cout << "small square: " << pow(bok + 2, 2) << endl;
+
+    return pow(bok + 2, 2);
 }
 
 int main()
@@ -88,9 +109,14 @@ int main()
     int k = stoi(ltrim(rtrim(k_temp)));
     */
 
-    vector<int> x = {-4, 3, 1};
-    vector<int> y = {3, -1, -2};
-    int k = 2;
+    // out 81
+    // vector<int> x = {0, 3};
+    // vector<int> y = {0, 7};
+
+    // out 36
+    vector<int> x = {0, 0, 1, 1, 2, 2};
+    vector<int> y = {0, 1, 0, 1, 0, 1};
+    int k = 4;
 
     long result = minArea(x, y, k);
 
