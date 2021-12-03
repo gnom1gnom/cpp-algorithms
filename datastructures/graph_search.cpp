68a0750ebfd92b0eff8c01ca4feb99cc4a826794
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2021-12-03 13:02:54 +0000

    fmt, search

--- datastructures/graph_search.cpp
+++ datastructures/graph_search.cpp
@@ -1,10 +1,24 @@
 // C++ program to print DFS traversal from
 // a given vertex in a given graph
+#define FMT_HEADER_ONLY
+
 #include <bits/stdc++.h>
+#include <fmt/core.h>
+#include <iostream>
+#include <string>
+#include <string_view>
+
 using namespace std;
 
 // Graph class represents a directed graph
 // using adjacency list representation
+
+enum state
+{
+    unvisited,
+    visited
+};
+
 class Graph
 {
 public:
@@ -13,7 +27,7 @@ public:
         this->root = root;
     }
     int root;
-    map<int, bool> visited;
+    map<int, state> visit_state;
     map<int, list<int>> adj;
 
     void addEdge(int v, int w)
@@ -31,8 +45,8 @@ public:
 
     void clearVisited()
     {
-        for (auto &elem : visited)
-            elem.second = false;
+        for (auto &elem : visit_state)
+            elem.second = unvisited;
     }
 
     // DFS traversal of the vertices
@@ -40,20 +54,22 @@ public:
     void DFS(int v);
 
     void BFS(int s);
+
+    bool BFS(int s, int e);
 };
 
 void Graph::DFS(int v)
 {
-    // Mark the current node as visited and
+    // Mark the current node as visit_state and
     // print it
-    visited[v] = true;
+    visit_state[v] = visited;
     cout << v << " ";
 
     // Recur for all the vertices adjacent
     // to this vertex
 
     for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (!visited[*i])
+        if (visit_state[*i] == unvisited)
             DFS(*i);
 }
 
@@ -62,8 +78,8 @@ void Graph::BFS(int s)
     // Create a queue for BFS
     list<int> queue;
 
-    // Mark the current node as visited and enqueue it
-    visited[s] = true;
+    // Mark the current node as visit_state and enqueue it
+    visit_state[s] = visited;
     queue.push_back(s);
 
     while (!queue.empty())
@@ -74,49 +90,95 @@ void Graph::BFS(int s)
         queue.pop_front();
 
         // Get all adjacent vertices of the dequeued
-        // vertex s. If a adjacent has not been visited,
-        // then mark it visited and enqueue it
+        // vertex s. If a adjacent has not been visit_state,
+        // then mark it visit_state and enqueue it
         for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
         {
-            if (!visited[*i])
+            if (visit_state[*i] == unvisited)
             {
-                visited[*i] = true;
+                visit_state[*i] = visited;
                 queue.push_back(*i);
             }
         }
     }
 }
 
+bool Graph::BFS(int s, int e)
+{
+    if (s == e)
+        return true;
+
+    // Create a queue for BFS
+    list<int> queue;
+
+    // Mark the current node as visit_state and enqueue it
+    // visit_state[s] = visiting;
+    queue.push_back(s);
+
+    while (!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " " << flush;
+        queue.pop_front();
+
+        // Get all adjacent vertices of the dequeued
+        // vertex s. If a adjacent has not been visit_state,
+        // then mark it visit_state and enqueue it
+        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if (visit_state[*i] == unvisited)
+            {
+                if (*i == e)
+                {
+                    cout << e << " " << flush;
+                    return true;
+                }
+                else
+                {
+                    // visit_state[*i] = visiting;
+                    queue.push_back(*i);
+                }
+            }
+            visit_state[s] = visited;
+        }
+    }
+    return false;
+}
+
 // Driver code
 int main()
 {
-    // Create a graph given in the above diagram
-    /*
-    Graph g(2);
-    g.addEdge(0, 1, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0, 3);
-    g.addEdge(3, 3);
-    */
+    /**
+     *  0 → 1 ← 2
+     *  ↓ ↘ ↓ ↘ ↑
+     *  5   4 ← 3
+     **/
 
     Graph g(0);
     g.addEdge(0, 1, 4, 5);
     g.addEdge(1, 3, 4);
     g.addEdge(3, 2, 4);
     // cycle
-    g.addEdge(2, 3);
+    g.addEdge(2, 1);
 
-    cout << "Depth First (starting from vertex 2) \n";
+    cout << "Depth First (starting from vertex 0) \n";
     g.clearVisited();
     g.DFS(g.root);
     cout << "\n";
 
-    cout << "Breadth First (starting from vertex 2) \n";
+    cout << "Breadth First (starting from vertex 0) \n";
     g.clearVisited();
     g.BFS(g.root);
     cout << "\n";
 
+    cout << "Searching path between 2 and 4 \n";
+    g.clearVisited();
+    fmt::print("{}\n", g.BFS(2, 4));
+
+    cout << "Searching path between 5 and 3 \n";
+    g.clearVisited();
+    fmt::print("{}\n", g.BFS(5, 3));
+
     return 0;
 }
-
-// improved by Vishnudev C
