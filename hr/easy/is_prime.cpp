67337a28a9769932b20b620785831112a0c27b9b
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2021-12-16 16:12:54 +0000

    ulepszona

--- hr/easy/is_prime.cpp
+++ hr/easy/is_prime.cpp
@@ -14,9 +14,27 @@ bool isPrime(long n)
     return true;
 }
 
+bool czyPierwsza(int n)
+{
+    if (n % 2 == 0)      // jeżeli n jest parzyste to nie może być liczbą pierwszą
+        return (n == 2); // czyba że n to 2 - jedyna liczba parzyszta i pierwsza jednocześnie
+
+    int pierw = sqrt(n);
+    // szukamy rozpoczynając od 3, i przeskakujemy przez wielokrotności 2 (bo wiemy że n nie jest parzyste)
+    for (int i = 3; i <= pierw; i += 2)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(int argc, char const *argv[])
 {
     int n = 23;
-    cout << n << ": " << isPrime(n) << endl;
+    cout << n << ": " << czyPierwsza(n) << endl;
     return 0;
 }
