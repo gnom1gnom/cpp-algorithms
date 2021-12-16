6e9b721d796ddf8a649e6b9b0b6f067aa3d56294
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2021-12-16 16:33:22 +0000

    wyjaśnienie o pierwiastku!

--- hr/easy/is_prime.cpp
+++ hr/easy/is_prime.cpp
@@ -19,7 +19,13 @@ bool czyPierwsza(int n)
     if (n % 2 == 0)      // jeżeli n jest parzyste to nie może być liczbą pierwszą
         return (n == 2); // czyba że n to 2 - jedyna liczba parzyszta i pierwsza jednocześnie
 
+    /**
+     * Przykładowo dla 12 dzielnikami są {1, 2, 3, 4, 6, 12} i prawdą jest to, że 1·12 = 2·6 = 3·4 = 12.
+     * Oznacza to, że nie trzeba wyszukiwać, aż do połowy liczby. Granicę przesuwamy na pierwiastek kwadratowy liczby a.
+     * Teraz jeśli będziemy szukać dzielników w zakresie [1, sqrt(a)] to znajdziemy wszystkie w zakresie [sqrt(a), a].
+     **/
     int pierw = sqrt(n);
+
     // szukamy rozpoczynając od 3, i przeskakujemy przez wielokrotności 2 (bo wiemy że n nie jest parzyste)
     for (int i = 3; i <= pierw; i += 2)
     {
